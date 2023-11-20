e0d262ee3da57609b1991192697fc9a2b49330ce
Milianigia Rios <nanirios671@gmail.com>
2023-11-20 09:06:59 -0600

    i think i am finished with the exercise? may need to refactor later.

--- src/main/java/com/codeup/codeupspringblog/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/PostController.java
@@ -1,30 +1,35 @@
 package com.codeup.codeupspringblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String postindex(){
-        return "posts index page";
+    public String postindex(Model model){
+        ArrayList<Post> posts = new ArrayList<>();
+        posts.add(new Post("First Post", "This is the content of the first post."));
+        posts.add(new Post("Second Post", "This is the content of the second post."));
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postId(){
-        return "view and individual post. ";
+    public String postId(Model model){
+        Post post = new Post("Single Post", "This is the content of the single post.");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
     @GetMapping("/posts/create")
-    @ResponseBody
     public String createForm(){
-        return "view the form for creating a post";
+        return "this shows the create post form";
     }
     @PostMapping("/posts/create")
-    @ResponseBody
     public String createPost(){
-        return "view the form for creating a post";
+        return "this shows the created post?";
     }
 }
